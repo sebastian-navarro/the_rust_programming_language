380584434d2846a5cadfc84bcedf8b1642d27d34
Sebastian Navarro <sebastian.navarro.saavedra@gmail.com>
2024-04-20 23:22:53 -0700

    Updating 17.2 OOP Traits

--- examples/17_2_oop_traits.rs
+++ examples/17_2_oop_traits.rs
@@ -2,14 +2,11 @@ pub trait Draw {
     fn draw(&self);
 }
 
-pub struct Screen<T: Draw> {
-    pub components: Vec<T>,
+pub struct Screen {
+    pub components : Vec<Box<dyn Draw>>, // <- components could be any type, is different than Generics
 }
 
-impl<T> Screen<T>
-where
-    T: Draw,
-{
+impl Screen {
     pub fn run(&self) {
         for component in self.components.iter() {
             component.draw();
@@ -17,5 +14,50 @@ where
     }
 }
 
+pub struct Button {
+    width:u32,
+    height:u32,
+    label: String,
+}
 
-fn main(){}
+impl Draw for Button {
+    fn draw(&self) {
+        
+    }
+}
+
+//Another type that implement draw button , but have different attributes. And could have another methods
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+fn main(){
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
