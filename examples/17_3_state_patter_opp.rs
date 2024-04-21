e4a8810463bef5f84397ec4f891fef55380868d3
Sebastian Navarro <sebastian.navarro.saavedra@gmail.com>
2024-04-20 23:56:44 -0700

    Added 17.3 State Pattern OOP

---
+++ examples/17_3_state_patter_opp.rs
@@ -0,0 +1,37 @@
+
+pub struct Post {
+    state : Option<Box<dyn State>> ,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state : Some(Box::new(Draft {})),
+            content: String::new()
+        }
+    }
+
+    pub fn add_new_text(&mut self, text: &str){
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
+}
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
+
+struct PendingReview {}
+
+impl State for PendingReview {}
+
+
+
+
+fn main() {}
